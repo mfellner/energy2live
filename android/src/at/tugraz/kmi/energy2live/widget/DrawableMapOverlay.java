5659900d6c086d24e050755abec869ef87988b51
Maximilian Fellner <max.fellner@gmail.com>
2012-01-16 01:04:29 +0100

    Formatting & refactoring.

--- android/src/at/tugraz/kmi/energy2live/widget/DrawableMapOverlay.java
+++ android/src/at/tugraz/kmi/energy2live/widget/DrawableMapOverlay.java
@@ -1,3 +1,18 @@
+/** Copyright 2011 Maximilian Fellner, Gernot Solic, Florian Sumann
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package at.tugraz.kmi.energy2live.widget;
 
 import android.content.Context;
@@ -11,27 +26,26 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
 public class DrawableMapOverlay extends Overlay {
-	private final GeoPoint geoPoint;
-	private final Context context;
-	private final int drawable;
+	private final GeoPoint mGeoPoint;
+	private final Point mPoint;
+	private final Bitmap mBitmap;
 
 	public DrawableMapOverlay(Context context, GeoPoint geoPoint, int drawable) {
-		this.context = context;
-		this.geoPoint = geoPoint;
-		this.drawable = drawable;
+		mGeoPoint = geoPoint;
+		mPoint = new Point();
+		mBitmap = BitmapFactory.decodeResource(context.getResources(), drawable);
+
 	}
 
 	@Override
 	public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
 		super.draw(canvas, mapView, shadow);
 
-		Point screenPoint = new Point();
-		mapView.getProjection().toPixels(geoPoint, screenPoint);
+		mapView.getProjection().toPixels(mGeoPoint, mPoint);
 
-		Bitmap markerImage = BitmapFactory.decodeResource(context.getResources(), drawable);
-
-		canvas.drawBitmap(markerImage, screenPoint.x - markerImage.getWidth() / 2,
-				screenPoint.y - markerImage.getHeight() / 2, null);
+		int x = mPoint.x - (mBitmap.getWidth() / 2);
+		int y = mPoint.y - (mBitmap.getHeight() / 2);
+		canvas.drawBitmap(mBitmap, x, y, null);
 		return true;
 	}
 
